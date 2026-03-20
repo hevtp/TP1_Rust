d9a84af3a03566cd44f70d675169fb66afcdc11a
hevtp <hevisindatop@gmail.com>
2026-03-20 13:59:50 +0100

    Changement mineurs commentaires

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 
 
 ///TP1 : Analyse de trames réseaux par Agathe Julien et Hevisinda Top
-
 ///Objectif : Identifier les trames Wifi d'identification et de localisation de drones et extraire les informations pertinentes de ces trames à l'aide d'un programme en Rust.
-
 /// Partie 1: Arguments de ligne de commande et documentation
-
 /// Cette partie permet de définir et gérer les différents
 /// arguments passés au programme en ligne de commande.
-
 ///La gestion des arguments est réalisée avec la bibliothèque clap
-
 ///On choisit d'utiliser le mode derive car plus lisible et intuitif.
 
 use clap::{Parser};
@@ -52,10 +47,8 @@ struct Args{
 }
 
 ///Partie 2 : Analyse du fichier PCAP avec Wireshark
-///
 /// Dans cette seconde partie, nous analysons le fichier de capture PCAP fourni sur Moodle à l'aide de Wireshark.
 /// La capture a été réalisée en mode "monitor" avec une carte Wi-Fi, ce qui permet de capturer toutes les trames présentes dans l’air, y compris les trames de gestion (management frames) et de contrôle.
-///
 /// On observe plusieurs types d'appareils présents sur le réseau (Apple, Huawei, Xiaomi, etc.), qui communiquent via le protocole **Wi-Fi 802.11**.
 ///
 /// Parmi les trames capturées, on distingue notamment :
@@ -70,10 +63,10 @@ struct Args{
 ///
 /// - **Trames Beacon** : envoyées en broadcast, c'est à dire à toutes les stations à portée. Ces trames annoncent la présence d’un réseau et contiennent des informations comme le **SSID** et l’**intervalle de beacon**.
 ///    Exemple : l’adresse MAC `86:2a:fd:a4:df:68` envoie une trame Beacon pour le réseau `"DIRECT-68-HP"`.
-
+///
 /// - **Intervalle de Beacon** : spécifie la durée entre les émissions successives de Beacon.
 ///  Il permet de savoir à quelle fréquence un client peut s’attendre à recevoir ces trames de balise.
-
+///
 /// - **Application dans le cas des drones**  : Dans le ca sdes drones, ce type de trame est utilisé par les drones pour signaler leur présence et leur position aux autorités. En analysant ces trames, on peut obtenir des informations sur les drones présents dans la zone de capture, telles que leur ID, leur position GPS, leur altitude, etc.
 /// Ces trames sont définies par l'arrêté publié au journal officiel et sont utilisées par les drones pour se conformer à la réglementation en vigueur en matière de sécurité aérienne.
 ///
@@ -88,13 +81,12 @@ struct Args{
 ///
 ///On identifie le DroneID :Tag: ID FR: 000 ENS EATHESEUS202300000000001
 ///Ses coordonnées : Latitude: -5863,96928, Tag: Longitude: 17394,57280
-///
-///
+
+
 /// Partie 3 : Analyse du fichier PCAP avec Rust.
 /// Il s'agit d'écrire un programme en Rust pour analyser ce fichier PCAP et extraire les informations pertinentes de ces trames "beacon" de localisation de drones.
 
 use pcap::Capture;
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cap = Capture::from_file("C:/Users/hevis/Downloads/Ressources pour le TP1-20260320/capture-23-05-08-ttgo.pcapng")?;
